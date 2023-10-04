f3d954c92a348a4aae69f15e23a8a9db21eff4f1
yuyueliang <2469536487@qq.com>
2023-10-04 16:39:36 -0700

    1st assignment

--- Bag.java
+++ Bag.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Bag {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bag<T> {
+    private ArrayList<T> items;
+
+    public Bag() {
+        this.items = new ArrayList<T>();
+    }
+
+    // Adds an item to the bag
+    public void add(T item) {
+        items.add(item);
+    }
+
+    // Clears all contents of the bag
+    public void clear() {
+        items.clear();
+    }
+
+    // Checks if the item exists in the bag
+    public boolean contains(T item) {
+        return items.contains(item);
+    }
+
+    // Returns a random element from the bag
+    public T grab() {
+        if(isEmpty()) return null; // Or throw an exception
+        Random rand = new Random();
+        return items.get(rand.nextInt(items.size()));
+    }
+
+    // Checks if the bag is empty
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    // Removes one instance of the item
+    public boolean remove(T item) {
+        return items.remove(item);
+    }
+
+    // Returns the number of items in the bag
+    public int size() {
+        return items.size();
+    }
+
+    // Returns an array with all the items
+    public Object[] toArray() {
+        return items.toArray();
+    }
+
+    // Returns a string representation of the bag
+    @Override
+    public String toString() {
+        return items.toString();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Bag<Integer> intBag = new Bag<>();
+
+        // Testing add method
+        intBag.add(5);
+        intBag.add(15);
+        intBag.add(10);
+        intBag.add(20);
+
+        System.out.println("Bag after adding elements: " + intBag.toString());
+
+        // Testing contains method
+        System.out.println("Bag contains 10: " + intBag.contains(10));
+        System.out.println("Bag contains 20: " + intBag.contains(20));
+
+        // Testing grab method
+        System.out.println("Random item: " + intBag.grab());
+
+        // Testing isEmpty method
+        System.out.println("Is bag empty?: " + intBag.isEmpty());
+
+        // Testing remove method
+        intBag.remove(10);
+        System.out.println("Bag after removing 10: " + intBag.toString());
+
+        // Testing size method
+        System.out.println("Size of bag: " + intBag.size());
+
+        // Testing toArray method
+        Object[] array = intBag.toArray();
+        System.out.println("Array from bag:");
+        for (Object obj : array) {
+            System.out.println(obj);
+        }
+
+        // Testing clear method
+        intBag.clear();
+        System.out.println("Bag after clearing: " + intBag.toString());
+    }
 }
